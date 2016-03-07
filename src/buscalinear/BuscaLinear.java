7520f3150bb1e5317b420174bb297cfcdb62f217
JEricFarias <ericfarrias@hotmail.com>
2016-03-07 14:12:58 -0300

    Busca linear generica

--- src/buscalinear/BuscaLinear.java
+++ src/buscalinear/BuscaLinear.java
@@ -3,8 +3,9 @@ package buscalinear;
 /**
  *
  * @author EricFarias
+ * @since 07/03/20016
  */
-public class BuscaLinear {
+public class BuscaLinear<T>{
     /**
      * Metodo estatico que retorna um indice de um vetor caso haja o valor especificado pelo usuário
      * caso o valor especificado não esteja contido dentro do vetor é retornado -1.
@@ -14,10 +15,12 @@ public class BuscaLinear {
      * 
      * @return o indice de que possui o valor especificado, caso o valor não exista é retornado o valor -1
      */
-    public static int buscar(int valor, int []vetor){
-        for(int i = 0; i < vetor.length; i++){
-            if(vetor[i] == valor){
-                return i;
+    public int buscar(Object valor, Object []vetor){
+        if(valor != null){
+            for(int i = 0; i < vetor.length; i++){
+                if(vetor[i].equals(valor)){
+                    return i;
+                }
             }
         }
         return -1;
